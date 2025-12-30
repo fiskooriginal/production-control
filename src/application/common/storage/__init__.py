e445f76818d5a93baef5797cbbd2321fe72d7391
Nikolai Bebikov <n.bebikov@comita.digital>
2025-12-30 17:01:24 +0300

    reports/fix: удален артефакт полученный при stash apply

--- src/application/common/storage/__init__.py
+++ src/application/common/storage/__init__.py
@@ -1,4 +1,3 @@
 from src.application.common.storage.interface import FileInfo, StorageServiceProtocol
-from src.application.common.storage.report_storage_adapter import ReportStorageAdapter
 
-__all__ = ["FileInfo", "ReportStorageAdapter", "StorageServiceProtocol"]
+__all__ = ["FileInfo", "StorageServiceProtocol"]
