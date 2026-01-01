59774d039f952307ab6455e013dd3a68f5d1baf7
Nikolai Bebikov <n.bebikov@comita.digital>
2026-01-01 15:09:55 +0000

    batches/refactor: удалён сервис проверки уникальности номера партии

--- src/domain/batches/services/validate_batch_number_uniqueness.py
+++
@@ -1,8 +0,0 @@
-from src.domain.batches.interfaces.repository import BatchRepositoryProtocol
-from src.domain.batches.value_objects import BatchNumber
-
-
-async def validate_batch_number_uniqueness(batch_number: BatchNumber, repository: BatchRepositoryProtocol) -> bool:
-    """Проверяет уникальность номера партии"""
-    existing_batch = await repository.get_by_batch_number(batch_number.value)
-    return existing_batch is None
